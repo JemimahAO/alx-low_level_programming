ae918bbf7350021c326224c23cda5e8735e4b699
Jemimah Adwar <jemimahadwar@gmail.com>
2023-04-10 05:49:40 -0700

    created file 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "main.h"
+
+/*
+*converting binary to unsigned int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+       	unsigned int total, pow;
+	int num;
+
+	if (b == NULL)
+		return (0);
+
+	for (num = 0; b[num]; num++)
+	{
+		if (b[num] != '0' && b[num] != '1')
+			return (0);
+	}
+
+	for (pow = 1, total = 0, num--; num >= 0; num--, pow *= 2)
+	{
+		if (b[num] == '1')
+			total += pow;
+	}
+
+	return (total);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+A readme for the 0x14-bit_manipulation c programming project
